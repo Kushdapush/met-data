22e2092572a2640d574f62f71f859cd3abfd87f6
Kushdapush <129064700+Kushdapush@users.noreply.github.com>
2024-12-04 19:17:37 +0530

    Update index.js
    Removes cors origin url

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
